9e6ecd8fde97ffa95dc57aee8d2ce55ecee10f84
vicperry <vic.hugx@gmail.com>
2022-06-06 15:10:42 -0300

    Home concluída, componente botão Link criado.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton'
+
+
 function Home(){
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem-vindos ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/new-project" text="Criar projeto"/>
+            <img src={savings} alt="Costs" />
+        </section>
     )
 }
 
